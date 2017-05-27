2a0eff9d981bb031baa5fb823827a069fffa1931
Simone Bonvicini <simone@Simones-MBP.lan>
2017-05-27 14:44:56 +0200

    first commit

---
+++ Travalert/app/src/main/java/simone/bonvicini/travalert/travalert/MainActivity.java
@@ -0,0 +1,24 @@
+package simone.bonvicini.travalert.travalert;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.maps_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,MapsActivity.class));
+            }
+        });
+    }
+}
